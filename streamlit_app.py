7f4f4f74d3a86783a11d8f5d1b040471036e749b
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2024-06-01 00:46:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,9 +36,8 @@ if user_input:
 # Donation button on the main page
 st.markdown(
     """
-    <a href='https://ko-fi.com/X7X47Q0EG' target='_blank'><img height='36' style='border:0px;height:36px;' 
-    src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>        
-    """,
+<script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script 
+type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#29abe0', 'C0C6YRSIF');kofiwidget2.draw();</script> """,
     unsafe_allow_html=True,
 )
 
